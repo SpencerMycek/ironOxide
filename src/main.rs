476d00f3d3ad09d48d2370d2ab60a43ad34a8c5c
Spencer Mycek <Spence.Mycek@gmail.com>
2020-07-26 18:01:55 -0400

    Adjust logging, and remove test logging lines

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 #![deny(warnings)]
 #![warn(rust_2018_idioms)]
 
+use pretty_env_logger;
+use log::LevelFilter;
+#[macro_use] extern crate log;
 
 #[tokio::main]
 async fn main() -> iron_oxide::Result<()> {
-    pretty_env_logger::init();
+    let mut builder = pretty_env_logger::formatted_builder();
+    builder.filter(None, LevelFilter::Info).init();
+
+    trace!("a trace example");
+    //debug!("deboogging");
+    //info!("such information");
+    //warn!("o_O");
+    //error!("boom");
 
     let running = iron_oxide::run();
     running.await
